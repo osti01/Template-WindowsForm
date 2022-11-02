e80737202814a72596b04dac9063ffb947dbd7b6
osti01 <osti01@wp.pl>
2022-11-03 00:05:15 +0100

    Initial

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 #ifndef UNICODE
 #define UNICODE
-#endif 
+#endif
 
+#include <iostream>
 #include <windows.h>
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
@@ -9,12 +10,12 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR pCmdLine, int nCmdShow)
 {
     // Register the window class.
-    const wchar_t CLASS_NAME[]  = L"Sample Window Class";
-    
-    WNDCLASS wc = { };
+    const wchar_t CLASS_NAME[] = L"Sample Window Class";
 
-    wc.lpfnWndProc   = WindowProc;
-    wc.hInstance     = hInstance;
+    WNDCLASS wc = {};
+
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = hInstance;
     wc.lpszClassName = CLASS_NAME;
 
     RegisterClass(&wc);
@@ -22,30 +23,33 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR pCmdLine,
     // Create the window.
 
     HWND hwnd = CreateWindowEx(
-        0,                              // Optional window styles.
-        CLASS_NAME,                     // Window class
-        L"Learn to Program Windows",    // Window text
-        WS_OVERLAPPEDWINDOW,            // Window style
+        0,                           // Optional window styles.
+        CLASS_NAME,                  // Window class
+        L"Learn to Program Windows", // Window text
+        WS_OVERLAPPEDWINDOW,         // Window style
 
         // Size and position
         CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
 
-        NULL,       // Parent window    
-        NULL,       // Menu
-        hInstance,  // Instance handle
-        NULL        // Additional application data
-        );
+        NULL,      // Parent window
+        NULL,      // Menu
+        hInstance, // Instance handle
+        NULL       // Additional application data
+    );
 
     if (hwnd == NULL)
     {
         return 0;
     }
 
+    HWND hButton1 = CreateWindow(L"Button", L"Button Window", WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON, 10, 10, 150, 20, hwnd, (HMENU)15, hInstance, NULL);
+
     ShowWindow(hwnd, nCmdShow);
+    UpdateWindow(hwnd);
 
     // Run the message loop.
 
-    MSG msg = { };
+    MSG msg = {};
     while (GetMessage(&msg, NULL, 0, 0) > 0)
     {
         TranslateMessage(&msg);
@@ -57,6 +61,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR pCmdLine,
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
+    UINT uID = LOWORD(wParam), uCmd = HIWORD(wParam);
     switch (uMsg)
     {
     case WM_DESTROY:
@@ -64,19 +69,31 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
         return 0;
 
     case WM_PAINT:
-        {
-            PAINTSTRUCT ps;
-            HDC hdc = BeginPaint(hwnd, &ps);
+    {
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hwnd, &ps);
 
-            // All painting occurs here, between BeginPaint and EndPaint.
+        // All painting occurs here, between BeginPaint and EndPaint.
 
-            FillRect(hdc, &ps.rcPaint, (HBRUSH) (COLOR_WINDOW+1));
+        FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
 
-            EndPaint(hwnd, &ps);
-        }
+        EndPaint(hwnd, &ps);
+
+        return 0;
+    }
+    case WM_COMMAND:
+
+        std::cout << "WM_Command entered" << std::endl;
+        std::cout << "uID: " << uID << " uCmd: " << uCmd << std::endl;
+
         return 0;
 
+    case WM_ACTIVATE:
+
+        std::cout << "Activate" << std::endl;
+        std::cout << "uID: " << uID << " uCmd: " << uCmd << std::endl;
+
+        return 0;
     }
     return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
-
